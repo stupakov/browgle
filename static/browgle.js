9c154e5d994a7649e0f6b719b2038f3647092a62
Ingy döt Net <ingy@ingy.net>
2009-12-09 11:59:32 -0800

    New stuff:
    * Roll dice
    * Sign off
    * Testing links
    * Moved old code out

--- static/browgle.js
+++ static/browgle.js
@@ -1,29 +1,35 @@
 (Browgle = function(){}).prototype = {
 
-    dice: [
-        ['A', 'A', 'C', 'I', 'O', 'T'],
-        ['A', 'B', 'I', 'L', 'T', 'Y'],
-        ['A', 'B', 'J', 'M', 'O', 'Qu'],
-        ['A', 'C', 'D', 'E', 'M', 'P'],
-        ['A', 'C', 'E', 'L', 'R', 'S'],
-        ['A', 'D', 'E', 'N', 'V', 'Z'],
-        ['A', 'H', 'M', 'O', 'R', 'S'],
-        ['B', 'F', 'I', 'O', 'R', 'X'],
-        ['D', 'E', 'N', 'S', 'O', 'W'],
-        ['D', 'K', 'N', 'O', 'U', 'T'],
-        ['E', 'E', 'F', 'H', 'I', 'Y'],
-        ['E', 'G', 'I', 'N', 'T', 'V'],
-        ['E', 'G', 'K', 'L', 'U', 'Y'],
-        ['E', 'H', 'I', 'N', 'P', 'S'],
-        ['E', 'L', 'P', 'S', 'T', 'U'],
-        ['G', 'I', 'L', 'R', 'U', 'W']
-    ],
+    currentUsers: [],
+
+    getDice: function() {
+        return [
+            ['A', 'A', 'C', 'I', 'O', 'T'],
+            ['A', 'B', 'I', 'L', 'T', 'Y'],
+            ['A', 'B', 'J', 'M', 'O', 'Qu'],
+            ['A', 'C', 'D', 'E', 'M', 'P'],
+            ['A', 'C', 'E', 'L', 'R', 'S'],
+            ['A', 'D', 'E', 'N', 'V', 'Z'],
+            ['A', 'H', 'M', 'O', 'R', 'S'],
+            ['B', 'F', 'I', 'O', 'R', 'X'],
+            ['D', 'E', 'N', 'S', 'O', 'W'],
+            ['D', 'K', 'N', 'O', 'U', 'T'],
+            ['E', 'E', 'F', 'H', 'I', 'Y'],
+            ['E', 'G', 'I', 'N', 'T', 'V'],
+            ['E', 'G', 'K', 'L', 'U', 'Y'],
+            ['E', 'H', 'I', 'N', 'P', 'S'],
+            ['E', 'L', 'P', 'S', 'T', 'U'],
+            ['G', 'I', 'L', 'R', 'U', 'W']
+        ];
+    },
 
     setup: function() {
+        this.xxxHooks();
 
         $('table.game_board td')
             .each( function(){this.innerHTML = "&nbsp;"} )
-            .height(50);
+            .height(50)
+            .width(50);
 
         var id = $.cookie('user_id');
         if ( !id ) {
@@ -39,22 +45,13 @@
     },
 
     showIdentity: function(id) {
-        var self = this;
         $('.user_list_pane').show();
         $('table.user_list tr')
             .each(function() {
                 $(this).append('<td></td>');
             });
         $('table.user_list tr').eq(0).find('td:last').get(0).innerHTML =
-            '<a href="#">' + id + '</a>';
-        $('table.user_list a')
-            .click(
-                function() {
-                    $.cookie('user_id', null);
-                    $('.user_list_pane').hide();
-                    self.getIdentity();
-                }
-            );
+            '<b>' + id + '</b>';
     },
 
     getIdentity: function() {
@@ -64,10 +61,11 @@
             .val('')
             .focus();
         $('form.user_id')
+            .unbind('submit')
             .submit(
                 function() {
                     var id = $(this).find('input').val();
-                    if (id.match(/[\w\.]+@[\w\.]+/)) {
+                    if (id.match(/^[\w\.\-]+@[\w\.\-]+\.\w{2,4}$/)) {
                         $.cookie('user_id', id);
                         $('.signin_pane').hide();
                         self.addUser(id);
@@ -87,72 +85,51 @@
         $('.user_list_pane').show();
     },
 
+    rollDice: function() {
+        dice = this.getDice();
+        var $slots = $('table.game_board td');
+        for (var i = 16; i > 0; i--) {
+            var ii = parseInt(Math.random() * i);
+            var die = dice.splice(ii, 1)[0];
+            var iii = parseInt(Math.random() * 6);
+            $slots[i - 1].textContent = die[iii];
+        }
+    },
+
+    checkWord: function(word) {
+        return true;
+    },
+
+    signOff: function() {
+        var id = $.cookie('user_id');
+
+        $('table.user_list tr:first td')
+            .each(function(i) {
+                if (this.textContent == id) {
+                    $('table.user_list tr')
+                        .find('td:eq(' + i + ')')
+                        .remove();
+                }
+            });
+                    
+        $.cookie('user_id', null);
+        $('.user_list_pane').hide();
+        this.getIdentity();
+    },
+
+    xxxHooks: function() {
+        var self = this;
+
+        $('.roll_dice').click(function() {
+            self.rollDice();
+            return false;
+        });
+
+        $('.sign_off').click(function() {
+            self.signOff();
+            return false;
+        });
+    },
+
+    'The': 'End'
 };
-
-/******************************************** Old Code
-
-var cookieName = 'tatsumaki_chat_ident';
-function doPost(el1, el) {
-  var ident = el1.attr('value');
-  if (ident) $.cookie(cookieName, ident, { path: '/chat' });
-  var text = el.attr('value');
-  if (!text) return;
-  $.ajax({
-    url: "/chat/foo/post",
-    data: { ident: ident, text: text },
-    type: 'post',
-    dataType: 'json',
-    success: function(r) { }
-  });
-  el.attr('value', '');
-  return;
-}
-
-function oldCode() {
-  var onNewEvent = function(e) {
-    try {
-      var src    = e.avatar || ("http://www.gravatar.com/avatar/" + $.md5(e.ident || 'foo'));
-      var name   = e.name   || e.ident || 'Anonymous';
-      var avatar = $('<img/>').attr('src', src).attr('alt', name);
-      if (e.ident) {
-        var link = e.ident.match(/https?:/) ? e.ident : 'mailto:' + e.ident;
-        avatar = $('<a/>').attr('href', link).attr('target', '_blank').append(avatar);
-      }
-      avatar = $('<td/>').addClass('avatar').append(avatar);
-
-      var message = $('<td/>').addClass('chat-message');
-      if (e.text) message.text(e.text);
-      if (e.html) message.html(e.html);
-      message.find('a').oembed(null, { embedMethod: "append", maxWidth: 500 });
-      var name = e.name || (e.ident ? e.ident.split('@')[0] : null);
-      if (name)
-        message.prepend($('<span/>').addClass('name').text(name+ ': '));
-
-      var date = new Date(e.time * 1000);
-      var meta = $('<td/>').addClass('meta').append(
-        '(' +
-        '<span class="pretty-time" title="' + date.toUTCString() + '">' + date.toDateString() + '</span>' +
-        ' from ' + e.address + ')'
-      );
-      $('.pretty-time', meta).prettyDate();
-      $('#messages').prepend($('<tr/>').addClass('message').append(avatar).append(message).append(meta));
-    } catch(e) { if (console) console.log(e) };
-  }
-
-  if (typeof DUI != 'undefined') {
-    var s = new DUI.Stream();
-    s.listen('application/json', function(payload) {
-      var event = eval('(' + payload + ')');
-      onNewEvent(event);
-    });
-    s.load('/chat/foo/mxhrpoll');
-  } else {
-    $.ev.handlers.message = onNewEvent;
-    $.ev.loop('/chat/foo/poll?client_id=' + Math.random());
-  }
-
-  if ($.cookie(cookieName))
-    $('#ident').attr('value', $.cookie(cookieName));
-
-  window.setInterval(function(){ $(".pretty-time").prettyDate() }, 1000 * 30);
-  */
